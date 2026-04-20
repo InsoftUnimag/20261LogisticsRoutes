5035a2db53d2159f65379fdc80e0d5c45ac016df
Jose Alejandro <214848445+JoseRodriguez0001@users.noreply.github.com>
2026-04-20 18:51:51 -0500

    deleted port in

--- src/main/java/com/logistics/routes/application/port/in/RegistrarConductorPort.java
+++
@@ -1,8 +0,0 @@
-package com.logistics.routes.application.port.in;
-
-import com.logistics.routes.application.command.RegistrarConductorCommand;
-import com.logistics.routes.domain.model.Conductor;
-
-public interface RegistrarConductorPort {
-    Conductor ejecutar(RegistrarConductorCommand command);
-}
